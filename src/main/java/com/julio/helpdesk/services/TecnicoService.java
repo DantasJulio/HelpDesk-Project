13866d623ea51cd09b72701627f95c0c3dc24952
Julio Dantas <juliodantas19@gmail.com>
2023-06-01 15:09:47 -0300

    PROJETO-00001 | Adicionando tratamento de exceção para o método GET da classe Tecnico | Criando as classes controladoras de exception.

--- src/main/java/com/julio/helpdesk/services/TecnicoService.java
+++ src/main/java/com/julio/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.julio.helpdesk.domain.Tecnico;
 import com.julio.helpdesk.repositories.TecnicoRepository;
+import com.julio.helpdesk.services.exception.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById (Integer id) {
 		Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
-		return tecnico.orElse(null);
+		return tecnico.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id)) ;
 	}
 
 }
